e099447bb487be6b0b9e311cf02982973184b0c3
Augusto Carmona <augustocarmonaperez@gmail.com>
2020-10-14 12:42:53 -0300

    inicio de la aplicacion del algoritmo quick sort en C

---
+++ algoritmo_quicksort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int quick_sort(arreglo);
+
+int main()
+{
+    int arreglo = {273,9,4,83,7,29,2,0,2,46,8,4,47,3,91,1,1,0,2,93,3,8,7,3,87,4,663,2};
+    printf("%d", quick_sort(arreglo));
+    return 0;
+}
+
+int quick_sort(arreglo)
+{
+    return 0;
+}
+
+/*
+iba a hacer una aplicacion del metodo quick sort tambien en C, y pense que seria más sencilla, pero no se me ocurre otra
+forma de "extraer" el pivot que usando punteros y TDAs, y como dije en el ultimo commit esto tambien me da paja
+ya fue, me dejo de joder y me voy a comer
+nos vemos mas tarde
+*/
