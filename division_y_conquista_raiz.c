5055fda525f0e69002c829ad2ce6909706421288
Augusto Carmona <augustocarmonaperez@gmail.com>
2020-10-31 00:13:27 -0300

    agrego practica del taller para el parcialito 1

---
+++ division_y_conquista_raiz.c
@@ -0,0 +1,9 @@
+/* 2:37:31
+    Ejercicio 6:
+
+    Implementar un algotimo que, por división y conquista, permita obtener la parte entera de la raíz cuadrada de un número n, en tiempo O(n log n).
+
+    Ejemplos:
+        - Para n = 10, debería devolver 3
+        - Para n = 25, debería devolver 5
+*/
---
+++ sumar_pares.c
@@ -0,0 +1,5 @@
+/* 2:47:04
+    Ejercicio 6:
+
+    Se tiene una lista en donde todos los elementos son punteros a números enteros. Implementar la función visitar para que calcula la duma de todos los números pares.
+*/
